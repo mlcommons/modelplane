94ac705ec4b110f69e2b967b6478a186dd2c0366
Vishal Doshi <vishal@mlcommons.org>
2025-06-23 13:56:16 -0400

    Test notebooks (#31)
    * Cleanup unnecessary env when bringing up mlflow.
    
    * USE_JUPYTER setting and startup script.
    
    * Refactor MLflow startup scripts to conditionally disable Jupyter support
    
    * Refactor GitHub Actions workflow for Jupyter testing and add notebook execution script
    
    * Update MLflow startup script to support detached mode and improve SSH handling
    
    * f
    
    * Fix Docker command in Jupyter test setup and update MLflow service start command
    
    * Only build mlflow when no jupyter.
    
    * f

---
+++ start_services.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# load .env
+set -a
+source .env
+set +a
+
+# Check if SSH agent is running
+if ssh-add -l >/dev/null 2>&1; then
+  SSH_FLAG="--ssh default"
+else
+  echo "SSH agent not running. Proceeding without SSH."
+  SSH_FLAG=""
+fi
+
+# Default values
+USE_JUPYTER=true
+DETACHED=""
+
+# Parse arguments
+for arg in "$@"; do
+  case $arg in
+    no-jupyter)
+      USE_JUPYTER=false
+      ;;
+    -d)
+      DETACHED="-d"
+      ;;
+  esac
+done
+
+# Start services based on the options
+if [ "$USE_JUPYTER" = "true" ]; then
+  docker compose down && docker compose build $SSH_FLAG && MLFLOW_TRACKING_URI="http://mlflow:8080" docker compose up $DETACHED
+else
+  docker compose down && docker compose build $SSH_FLAG mlflow && MLFLOW_TRACKING_URI="http://localhost:8080" docker compose up $DETACHED mlflow
+fi
---
+++ tests/notebooks/run.py
@@ -0,0 +1,31 @@
+# This is tested via GitHub Actions (within the Jupyter container), but we
+# don't want pytest to run it directly, hence the strange name.
+
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+
+def test_notebooks(notebooks_dir):
+    notebooks = [f for f in os.listdir(notebooks_dir) if f.endswith(".ipynb")]
+    if not notebooks:
+        print(f"No notebooks found in {notebooks_dir}")
+        return
+
+    for notebook in notebooks:
+        notebook_path = os.path.join(notebooks_dir, notebook)
+        print(f"Testing notebook: {notebook_path}")
+        with open(notebook_path) as f:
+            nb = nbformat.read(f, as_version=4)
+            ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+            try:
+                ep.preprocess(nb, {"metadata": {"path": notebooks_dir}})
+                print(f"Notebook {notebook} executed successfully.")
+            except Exception as e:
+                print(f"Error executing notebook {notebook}: {e}")
+                raise
+
+
+if __name__ == "__main__":
+    notebooks_dir = "/app/flightpaths"
+    test_notebooks(notebooks_dir)
