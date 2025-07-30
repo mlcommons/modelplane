fc759be3d03ea0bfdcc1c5c4bc79749bb2160ac7
Vishal Doshi <vishal@mlcommons.org>
2025-07-30 17:14:26 -0400

    Update lister with compact_uid_list syntax.

--- src/modelplane/runways/lister.py
+++ src/modelplane/runways/lister.py
@@ -1,16 +1,15 @@
 from modelgauge.annotator_registry import ANNOTATORS
-from modelgauge.command_line import compact_sut_list
 from modelgauge.ensemble_annotator_set import ENSEMBLE_STRATEGIES
 from modelgauge.sut_registry import SUTS
 
 
 def list_annotators():
-    print(compact_sut_list(ANNOTATORS))
+    print(ANNOTATORS.compact_uid_list())
 
 
 def list_suts():
-    print(compact_sut_list(SUTS))
+    print(SUTS.compact_uid_list())
 
 
 def list_ensemble_strategies():
-    print(compact_sut_list(ENSEMBLE_STRATEGIES))
+    print(sorted(ENSEMBLE_STRATEGIES))
